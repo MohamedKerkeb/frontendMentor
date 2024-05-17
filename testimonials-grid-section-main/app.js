711a02f32d1b2b0a2e6d14f86deab6352b441569
Kerkeb Mohamed <kerkeb.moha@gmail.com>
2024-05-17 11:24:25 +0200

    Setting up Tailwind CSS and beginning development.

---
+++ testimonials-grid-section-main/app.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        ModerateViolet: "hsl(263, 55%, 52%)",
+        VeryDarkGrayishBlue: "hsl(217, 19%, 35%)",
+        VeryDarkBlackishBlue: "hsl(219, 29%, 14%)",
+        White: "hsl(0, 0%, 100%)",
+        LightGray: "hsl(0, 0%, 81%)",
+        LightGrayishBlue: "hsl(210, 46%, 95%)",
+      },
+      backgroundImage: {
+        quotation: "url('images/bg-pattern-quotation.svg')",
+      },
+    },
+    backgroundPosition: {
+      "right-top": "right 6rem top ",
+    },
+  },
+};
