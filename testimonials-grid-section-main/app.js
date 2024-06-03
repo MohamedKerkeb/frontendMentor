984e53fed9b398ba944cd3f7262f6c35c7d2cb53
Kerkeb Mohamed <kerkeb.moha@gmail.com>
2024-06-03 22:24:17 +0200

    Implemented all planned features

--- testimonials-grid-section-main/app.js
+++ testimonials-grid-section-main/app.js
@@ -2,19 +2,20 @@ tailwind.config = {
   theme: {
     extend: {
       colors: {
-        ModerateViolet: "hsl(263, 55%, 52%)",
-        VeryDarkGrayishBlue: "hsl(217, 19%, 35%)",
-        VeryDarkBlackishBlue: "hsl(219, 29%, 14%)",
-        White: "hsl(0, 0%, 100%)",
-        LightGray: "hsl(0, 0%, 81%)",
-        LightGrayishBlue: "hsl(210, 46%, 95%)",
+        moderateViolet: "hsl(263, 55%, 52%)",
+        veryDarkGrayishBlue: "hsl(217, 19%, 35%)",
+        veryDarkBlackishBlue: "hsl(219, 29%, 14%)",
+        white: "hsl(0, 0%, 100%)",
+        lightGray: "hsl(0, 0%, 81%)",
+        lightGrayishBlue: "hsl(210, 46%, 95%)",
       },
       backgroundImage: {
         quotation: "url('images/bg-pattern-quotation.svg')",
       },
     },
     backgroundPosition: {
-      "right-top": "right 6rem top ",
+      "right-top-lg": "right 6rem top ",
+      "right-top": "right 2rem top",
     },
   },
 };
